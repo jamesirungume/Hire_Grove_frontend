21d286f15a3424990e50e6dd55cd2bb529767564
jamesirungume <134966859+jamesirungume@users.noreply.github.com>
2023-11-01 09:22:03 +0300

    Update JobListings.jsx

--- src/JobListings.jsx
+++ src/JobListings.jsx
@@ -1,82 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import JobListing from './JobListing';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function JobListings() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [locationFilter, setLocationFilter] = useState("");
+const JobListings = () => {
   const [jobListings, setJobListings] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Retrieve the access token from local storage
-    const accessToken = localStorage.getItem('loginToken'); // Change 'access_token' to 'loginToken' if that's what you're using in local storage
+    const token = localStorage.getItem('token');
 
-    // Fetch the job listings from the backend API with authentication
-    fetch("https://hire-backend.onrender.com/Availablejobs", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json', // Add this line to specify JSON content type
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
+    if (token) {
+      const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+        // Add any other necessary headers here
+      };
+
+      axios.get('https://your-api-endpoint.com/your-endpoint', {
+        headers: headers
       })
-      .then((jobListings) => {
-        console.log("Fetched job listings:", jobListings);
-        setJobListings(jobListings);
+      .then(response => {
+        console.log('Response:', response.data);
+        setJobListings(response.data); // Assuming the response contains job listings
         setLoading(false);
       })
-      .catch((error) => {
-        console.error("Error fetching job listings:", error);
+      .catch(error => {
+        console.error('Error:', error);
         setError(error);
         setLoading(false);
       });
+    } else {
+      console.log('Token not found in local storage');
+      setLoading(false);
+      // Handle if token is not available
+    }
   }, []);
 
   return (
     <>
-      <div className="jobsearch-nav-container">
-        <form action="" className="jobsearch-form">
-          <input
-            placeholder="job title"
-            className="search-jobs"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </form>
-        <div className="job-nav-links">
-          <Link to="/jobs/appliedjobs">Applied Jobs</Link>
-          <Link to="/addjob" className="create-job-alert">
-            Create Job Alert
-          </Link>
+      {loading ? (
+        <p>Loading job listings...</p>
+      ) : error ? (
+        <p>Error fetching job listings. Please try again.</p>
+      ) : (
+        <div>
+          <h1>Job Listings</h1>
+          <ul>
+            {jobListings.map((job, index) => (
+              <li key={index}>
+                {/* Display job details as needed */}
+                <div>
+                  <strong>{job.title}</strong>
+                  <p>{job.description}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
-
-      <div className="job-listings">
-        {/* Conditional rendering to display the jobs */}
-        {jobListings !== '' && jobListings.length > 0 ? (
-          searchTerm !== ''
-            ? filteredJobs.map((job, index) => (
-                <JobListing key={index} job={job} />
-              ))
-            : (
-              <ul>
-                {jobListings.map((job, index) => (
-                  <JobListing key={index} job={job} />
-                ))}
-              </ul>
-            )
-        ) : (
-          <p>Loading job listings...</p>
-        )}
-      </div>
+      )}
     </>
   );
-}
+};
 
 export default JobListings;
